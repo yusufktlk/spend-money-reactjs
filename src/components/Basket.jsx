74ef45fb03f06fdd88dc0a8ae5dff27ee8f4907e
Yusuf Kıtlık <71187794+yusufktlk@users.noreply.github.com>
2023-07-14 14:41:26 +0300

    Update Basket.jsx

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -9,7 +9,7 @@ function Basket({basket,products,total}) {
                 <BasketList total={total} item={item} product={products.find(product => product.id === item.id)} />
         ))} 
         
-        <h1 className='mt-5 text-4xl text-red-600'>Total: {total}</h1>
+        <h1 className='mt-5 text-4xl text-red-600'>Total: {new Intl.NumberFormat().format(total)}</h1>
     </div>
   )
 }
